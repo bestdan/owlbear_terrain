a47f5de3eea406cdc28ab607dc5de30a624fbc18
Dan Egan <dp.egan@gmail.com>
2025-11-30 10:37:09 -0500

    local vite development

--- vite.config.js
+++ vite.config.js
@@ -13,7 +13,10 @@ export default defineConfig({
   },
   server: {
     port: 3000,
-    open: false
+    open: false,
+    cors: {
+      origin: "https://www.owlbear.rodeo",
+    },
   },
   publicDir: 'public'
 });
